e49b67433563e1240097a015c947663cc1b4d526
Pankaj Anuragi <anuragi.pankaj@tftus.com>
2022-05-05 23:42:18 +0530

    added custom changes

--- routes/data.js
+++ routes/data.js
@@ -17,8 +17,7 @@ var config = {
 /* GET quotes listing. */
 router.get('/', async function (req, res, next) {
   try {
-    res.json((await axios(config)));
-    //res.json((["data", "stock"]));
+    res.json((await axios(config)).data);
   } catch (err) {
     console.error(`Error while getting data `, err.message);
     res.status(err.statusCode || 500).json({ 'message': err.message });
